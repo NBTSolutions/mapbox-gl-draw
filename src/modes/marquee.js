0868d2cff18ecc47407fb2cc7bd3cd038fe7ac54
Michael Simms <michaelsimms@Michaels-MacBook-Pro.local>
2026-04-15 13:44:10 -0400

    really forcefully disable drag

--- src/modes/marquee.js
+++ src/modes/marquee.js
@@ -19,9 +19,13 @@ RectangularDraw.onSetup = function () {
   this.clearSelectedFeatures();
   doubleClickZoom.disable(this);
 
-  setTimeout(() => {
-    if (!this.map || !this.map.dragPan) return;
-    this.map.dragPan.disable();
+  // I am so so sorry. Unfortunately "stopExtendedInteractions" in simple select can still sometimes be triggered asyncronously AFTER
+  // we enter freehand mode. The timing of it is erratic, and the cause is unclear.
+  [0, 20, 80, 120].forEach((t) => {
+    setTimeout(() => {
+      if (!this.map || !this.map.dragPan) return;
+      this.map.dragPan.disable();
+    }, t);
   });
 
   this.setActionableState({
